33afb36b76f3bb890e99d71d4cb4eba344286ee4
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-03-20 19:17:29 -0300

    consumindo api

--- src/App.ts
+++ src/App.ts
@@ -4,6 +4,7 @@ export const Container = styled.div`
   background: #202632;
   height: 100vh;
   padding: 12rem 1rem;
+  min-width: 37rem;
 
   text-align: center;
 
@@ -16,6 +17,7 @@ export const Container = styled.div`
       color: #4EE69D;
       font-size: 1.2rem;
       margin: 1rem auto 2rem;
+      letter-spacing: 2px;
     }
 
     p{
@@ -27,6 +29,25 @@ export const Container = styled.div`
       margin: 1.5rem auto 1.5rem;
     }
   }
+
+  @media(min-width: 1024px){
+    padding: 17rem 15rem;
+
+    .card{
+      width: 53.6rem;
+      height: 33.3rem;
+      margin: 0 auto;
+    }
+    
+    .line{
+      background-image: url(./assets/pattern-divider-desktop.svg);
+    }
+
+    .line .imgLine{
+      display: none;
+    }
+    
+  }
 `;
 
 export const Button = styled.button`
@@ -36,9 +57,22 @@ export const Button = styled.button`
   margin: 0 auto -3.5rem;
   background: #53FFAB;
   border: none;
+  cursor: pointer;
   
   .imgIcon{
     width: 1.6rem;
     height: 1.6rem;
   }
+
+  box-shadow: transparent;
+  border: 1px solid #ffffff80;
+  transition: 1.5s;
+  user-select: none;
+  
+  :hover,
+  :focus {
+    background: #008cff;
+    box-shadow: 0 0 50px #53FFAB, 0 0 20px #53FFAB, 0 0 50px #53FFAB, 0 0 100px #53FFAB;
+  }
+
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
 import { Container, Button } from "./App";
 import icon from './assets/icon-dice.svg';
 import divMobile from './assets/pattern-divider-mobile.svg';
+import { api } from "./service/api.service";
+import { useState, useEffect } from "react";
+
 
 export const App = () => {
+
+  const [advice, setAdvice] = useState<any>({});
+
+  const Refresh = async() =>{
+    try {
+      const response = await api.get(`/advice`)
+      
+      const data = response.data.slip;
+
+      console.log(data);
+
+      setAdvice(data);
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    
+    Refresh()
+  }, [])
+
   return (
     <Container>
       <div className="card">
-        <h1>ADVICE <span>#117'</span></h1>
-
-        <p>"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-          it to make a type specimen book."
-        </p>
+        <h1>ADVICE <span>#{advice.id}</span></h1>
+        {!advice.advice ? (
+          <p>Loading...</p>
+        ):(
+          <p>
+            {advice.advice}
+          </p>
+        )}
+        
         <div className="line">
-          <img 
+          <img
+            className="imgLine" 
             src={divMobile}
           /> 
         </div>
-        <Button>
+        <Button onClick={Refresh}>
           <img
             className="imgIcon"
             src={icon}
